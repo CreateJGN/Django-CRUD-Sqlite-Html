5f9110c5337416d290882a4b2909bfcfe3e833ab
CreateJGN <newjgn@protonmail.com>
2024-08-15 08:45:27 +0900

    created a new edit page and then realized edit feature.

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Members
+from django.urls import reverse
 
 def index(request):
     mymembers = Members.objects.all().values()
@@ -9,3 +10,14 @@ def index(request):
         'mymembers' : mymembers
     }
     return HttpResponse(template.render(context, request))
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
