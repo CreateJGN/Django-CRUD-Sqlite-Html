c3f432034689e7a2af24eec55b8ae6fe8e05ea41
CreateJGN <newjgn@protonmail.com>
2024-08-14 11:08:46 +0900

    Create model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+class Members(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
